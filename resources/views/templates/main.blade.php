9a9467718f06cd043406e9f855b4de8ddc1e3607
Sayo Alagbe <smalmic@yahoo.com>
2023-05-22 00:07:42 +0100

    Add main site link to menu

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -23,7 +23,10 @@
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('https://newnigeriadiaspora.com/')}}">Main site</a>
+                    </li>
+                    <li class="nav-item">
                     <a class="nav-link" href="{{ url('/policy-ideas')}}">Policy Ideas</a>
                   </li>
                   <li class="nav-item">
